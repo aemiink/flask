449b65e7f1eccab85ff25ef25055a499ca1e4659
Emin <102097443+aemiink@users.noreply.github.com>
2023-11-20 18:49:03 +0300

    Update main.py

--- main.py
+++ main.py
@@ -7,4 +7,8 @@ def ogrenciler(sayi=5):
     return render_template("index.html",
                            sayi=sayi)
 
+@app.route("/")
+def home():
+    return render_template("home.html")
+
 app.run(debug=True)
